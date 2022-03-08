89aae4e7babe8790af17b79d174972cc35f7e881
suhaimaislam <73371083+suhaimaislam@users.noreply.github.com>
2022-03-07 23:12:14 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -3,14 +3,30 @@ from app import db
 
 class User(db.Model):
     __tablename__ = "users"
-
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False)
+    posts = db.relationship("Post", back_populates='user')
+    
     def serialize(self):
-        return {}
+        return {
+            "id": self.id,
+            "username": self.username,
+            "posts": [item.serialize() for item in self.posts]
+        }
 
 
 class Post(db.Model):
     __tablename__ = "posts"
-
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(280), nullable=False)
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship("User", back_populates="posts")
 
     def serialize(self):
-        return {}
+        return {
+            "id": self.id,
+            "content": self.content, 
+            "user_id": self.user_id,
+            "user": self.user.username,
+        }
